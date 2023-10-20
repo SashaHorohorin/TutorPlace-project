1d6bb6c2239fee16c4b5545a478c7a983f6fac16
Александр Хорохорин <aleksandrhorohorin@MacBook-Air-Aleksandr.local>
2023-10-20 22:07:04 +0300

    finish project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import MainPage from './components/MainPage/MainPage';
+import TrainPage from './components/TrainPage/TrainPage';
+
+function App() {
+  return (
+    <div className="App">
+      <MainPage/>
+      <TrainPage/>
+    </div>
+  );
+}
+
+export default App;
